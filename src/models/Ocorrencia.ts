0253d0af8875a8574b2590e9bff1bf5647198a76
José Arthur Braz <arthurbrajo078@gmail.com>
2025-06-16 16:29:10 -0300

    Delete src directory

--- src/models/Ocorrencia.ts
+++
@@ -1,9 +0,0 @@
-import { Aluno } from "./Aluno";
-import { Professor } from "./Professor";
-
-export interface Ocorrencia {
-  aluno: Aluno;
-  professor: Professor;
-  aula: string;
-  motivo: string;
-}
--- src/models/Professor.ts
+++
@@ -1,9 +0,0 @@
-// src/models/Professor.ts
-import { Pessoa } from "./Pessoa";
-
-export class Professor extends Pessoa {
-  constructor(nome: string, idade: number, public disciplina: string) {
-    super(nome, idade);
-  }
-}
-// O Professor herda de Pessoa e adiciona a propriedade disciplina
